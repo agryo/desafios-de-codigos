c5bc27f5793c680e9e7d3edf2494b0fdee3baf2d
Agryo Araujo <agryostallion@gmail.com>
2023-02-16 22:08:30 -0300

    Testando o Github

--- Projeto/Projeto1/src/br/com/agryo/PrimeiroPrograma.java
+++ Projeto/Projeto1/src/br/com/agryo/PrimeiroPrograma.java
@@ -6,6 +6,7 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
         System.out.println(gato);
+        System.out.println(gato);
         /*int a = 10;
         int b = 15;
         System.out.println("Hello World! " + (a+b));*/
