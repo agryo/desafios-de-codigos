34a89f955ac83828f4b01cca7b39127f2aa1e7d4
Agryo Araujo <agryostallion@gmail.com>
2023-02-18 00:40:21 -0300

    Implementando a SmartTV

---
+++ Projeto/Projeto1/src/br/com/agryo/smarttv/SmartTv.java
@@ -0,0 +1,32 @@
+package br.com.agryo.smarttv;
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+    public void subirCanal(){
+        canal++;
+        System.out.println("Mudou para o canal: " + canal);
+    }
+    public void descerCanal(){
+        canal--;
+        System.out.println("Mudou para o canal: " + canal);
+    }
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+        System.out.println("Mudou para o canal: " + canal);
+    }
+    public void ligar(){
+        ligada = true;
+    }
+    public void desligar(){
+        ligada = false;
+    }
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("Aumentando volume para: " + volume);
+    }
+    public void baixarVolume(){
+        volume--;
+        System.out.println("Baixando volume para: " + volume);
+    }
+}
---
+++ Projeto/Projeto1/src/br/com/agryo/smarttv/Usuario.java
@@ -0,0 +1,28 @@
+package br.com.agryo.smarttv;
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("Está ligada? " + smartTv.ligada);
+        System.out.println("Qual o canal? " + smartTv.canal);
+        System.out.println("Qual volume atual? " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status -> Está ligada? " + smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.baixarVolume();
+
+        smartTv.mudarCanal(15);
+        smartTv.subirCanal();
+        smartTv.subirCanal();
+        smartTv.descerCanal();
+        smartTv.mudarCanal(8);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> Está ligada? " + smartTv.ligada);
+    }
+}
