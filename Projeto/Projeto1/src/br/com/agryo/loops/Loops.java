d9c7900ee3145b49fcccbd4dbb525bedbf88bd18
Agryo Araujo <agryostallion@gmail.com>
2023-02-20 23:56:29 -0300

    Exercicio sobre Loops

---
+++ Projeto/Projeto1/src/br/com/agryo/loops/Loops.java
@@ -0,0 +1,28 @@
+package br.com.agryo.loops;
+
+import java.util.Scanner;
+
+/*
+Faça um programa que receba dois valores de dados no teclado,
+o primeiro representa um nome de aluno e o outro a idade do aluno,
+e ele irá repetir enquanto o usuario desejar.
+(O programa só para quando o campo nome recebet o numero 0 a desejo do usuario)
+ */
+public class Loops {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+        while (true) {
+            System.out.println("Digite o Nome: ");
+            nome = scan.next();
+            if (nome.equals("0")) break;
+
+            System.out.println("Digite a Idade: ");
+            idade = scan.nextInt();
+        }
+
+    }
+}
