6e6324623f79ab96134f21cde45d903b44638c53
Agryo Araujo <agryostallion@gmail.com>
2023-02-20 21:59:12 -0300

    Exercícios finalizado

--- Projeto/Projeto1/src/br/com/agryo/operadoresrelacionais/Switch.java
+++ Projeto/Projeto1/src/br/com/agryo/operadoresrelacionais/Switch.java
@@ -1,16 +1,24 @@
 package br.com.agryo.operadoresrelacionais;
 
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
 import java.util.Scanner;
+import java.util.spi.AbstractResourceBundleProvider;
 
 public class Switch {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         String diaSemana;
+        int numero;
         System.out.println("Segundo exercicio sobre SWITCH!");
         System.out.println("Digite o dia da semana para informar o número correspondente: ");
         diaSemana = scan.nextLine();
         numeroDia(diaSemana);
+        numero = pegaNumero(diaSemana);
+        System.out.println("Dia: " + numero);
+        bonus(numero);
+        ferias(numero);
     }
     public static void numeroDia(String diaSemana){
         switch (diaSemana) {
@@ -39,6 +47,62 @@ public class Switch {
                 System.out.println("Digite corretamente o dia com acentuação e apenas a primeira letra maiuscula!");
                 break;
         }
+    }
+    public static int pegaNumero(String dia){
+        int numero = 0;
+        switch (dia) {
+            case ("Domingo"):
+                numero = 1;
+                break;
+            case ("Segunda"):
+                numero = 2;
+                break;
+            case ("Terça"):
+                numero = 3;
+                break;
+            case ("Quarta"):
+                numero = 4;
+                break;
+            case ("Quinta"):
+                numero = 5;
+                break;
+            case ("Sexta"):
+                numero = 6;
+                break;
+            case ("Sábado"):
+                numero = 7;
+                break;
+        }
+        return numero;
+    }
+    public static void bonus(int numero){
+        switch (numero) {
+            case 1:
+            case 2:
+            case 3:
+                System.out.println("Certo!");
+                break;
+            case 4:
+                System.out.println("Errado!");
+                break;
+            case 5:
+                System.out.println("Talvez!");
+                break;
+            default:
+                System.out.println("Valor indefinido!");
+        }
 
+    }
+    public static void ferias(int numero){
+        switch (numero){
+            case 1:
+            case 6:
+            case 7:
+                System.out.println("Você está de FERIAS!");
+                break;
+            default:
+                System.out.println("Você está trabalhando...");
+                break;
+        }
     }
 }
