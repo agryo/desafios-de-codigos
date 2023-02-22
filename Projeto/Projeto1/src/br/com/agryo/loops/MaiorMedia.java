0beb1a54bd80312d7ea2423e0a4464ed9b60231f
Agryo Araujo <agryostallion@gmail.com>
2023-02-22 01:47:31 -0300

    Exercicio sobre While e Do While

---
+++ Projeto/Projeto1/src/br/com/agryo/loops/MaiorMedia.java
@@ -0,0 +1,30 @@
+package br.com.agryo.loops;
+
+import java.util.Scanner;
+
+public class MaiorMedia {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int contador = 5;
+        double numero;
+        double soma = 0d;
+        double maior = 0d;
+
+        System.out.println("----------------------------------------------------------");
+        System.out.println("Digite 5 numeros que vou informar o Maior e a Média deles.");
+        System.out.println("----------------------------------------------------------");
+
+        do {
+            System.out.println("Faltam " + contador + ", digite o numero: ");
+            numero = scan.nextDouble();
+            contador--;
+            soma = soma + numero;
+            if (numero > maior) maior = numero;
+        } while (contador != 0);
+
+        System.out.println("O maior numéro foi: " + maior);
+        System.out.println("E a Média dos números foi: " + (soma/5));
+        System.out.println("Finalizado!");
+    }
+}
