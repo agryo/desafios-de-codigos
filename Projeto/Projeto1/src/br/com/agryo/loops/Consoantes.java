58c5b6698e2e402acf8e41f42a3572f5155eb687
Agryo Araujo <agryostallion@gmail.com>
2023-02-26 20:09:05 -0300

    Exercicio sobre vetores consoantes

---
+++ Projeto/Projeto1/src/br/com/agryo/loops/Consoantes.java
@@ -0,0 +1,40 @@
+package br.com.agryo.loops;
+
+import java.util.Scanner;
+
+public class Consoantes {
+    public static void main(String[] args) {
+        Scanner digitar = new Scanner(System.in);
+
+        String[] consoantes = new String[6];
+        int quantidadeCosoantes = 0;
+        int contador = 0;
+
+        System.out.println("O tamanho do vetor é: " + consoantes.length);
+
+        do{
+            System.out.println("Digite uma letra: ");
+            String letra = digitar.next();
+
+            if ( !(letra.equalsIgnoreCase("a") |
+                    letra.equalsIgnoreCase("e") |
+                    letra.equalsIgnoreCase("i") |
+                    letra.equalsIgnoreCase("o") |
+                    letra.equalsIgnoreCase("u")) ){
+                consoantes[contador] = letra;
+                quantidadeCosoantes++;
+            }
+
+            contador++;
+        }while (contador < consoantes.length);
+
+        System.out.println("As consoantes desse vetor são: ");
+        for ( String consoante : consoantes ){
+            if (consoante != null){
+                System.out.print(consoante + " ");
+            }
+        }
+
+        System.out.println("\nA quantidade de consoantes foi: " + quantidadeCosoantes);
+    }
+}
