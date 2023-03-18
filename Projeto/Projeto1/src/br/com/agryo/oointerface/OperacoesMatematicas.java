8329cc1d87b35ba9d35d9aef91ee3dbe5184b90c
Agryo Araujo <agryostallion@gmail.com>
2023-03-18 00:14:18 -0300

    Exercicio sobre Interfaces

---
+++ Projeto/Projeto1/src/br/com/agryo/oointerface/OperacoesMatematicas.java
@@ -0,0 +1,8 @@
+package br.com.agryo.oointerface;
+
+public interface OperacoesMatematicas {
+    void soma(double operando1, double operando2);
+    void subtracao(double operando1, double operando2);
+    void multiplicacao(double operando1, double operando2);
+    void divisao(double operando1, double operando2);
+}
