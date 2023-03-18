32e1d1773612065ec62c5487da584de5868ca2ba
Agryo Araujo <agryostallion@gmail.com>
2023-03-17 23:35:40 -0300

    Exercicio sobre Herança

---
+++ Projeto/Projeto1/src/br/com/agryo/oopolimorfiscosobrescrita/ClasseFilha1.java
@@ -0,0 +1,8 @@
+package br.com.agryo.oopolimorfiscosobrescrita;
+
+public class ClasseFilha1 extends ClasseMae{
+    @Override
+    void metodo1(){
+        System.out.println("Método 1 da Classe Filha 1.");
+    }
+}
